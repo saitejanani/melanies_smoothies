18d24833818ca6a346206ad39f2f3157775b303a
saitejanani <85742496+saitejanani@users.noreply.github.com>
2026-04-09 13:07:35 +0530

    Update dataframe handling and import pandas
    Converted Snowpark dataframe to Pandas for use with LOC function and updated dataframe display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests  
+import requests 
+import pandas as pd
 # Write directly to the app
 st.title( ":cup_with_straw: Hey Buddies! Customize Your Smoothies:cup_with_straw:")
 st.write(
@@ -14,7 +15,12 @@ st.write('The Name on your Smoothie will be :', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+# Convert the snowpark dataframe to a pandas dataframe so we can use the LOC Function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',my_dataframe,max_selections=5)
